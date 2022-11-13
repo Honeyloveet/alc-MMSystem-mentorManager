49a05e80c32059635d9e8e32f23d8b84a00dfb8e
Samuel Kebede <honeyloveet@gmail.com>
2022-11-13 16:22:16 +0300

    Moved LoginViewModel.kt to viewmodel package.

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/LoginViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.peculiaruc.alc_mmsystem_mentormanager.ui.fragments
-
-import androidx.lifecycle.ViewModel
-
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
