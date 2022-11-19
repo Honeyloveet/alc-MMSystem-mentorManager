20c20b7e641d0b999c99fabaf434356a3d5a56c6
Ahmed Teleb <ahmedasd37@gmail.com>
2022-11-19 02:06:03 +0200

    edit profile screen design finish

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/ProfileFragment.kt
@@ -6,7 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.peculiaruc.alc_mmsystem_mentormanager.R
+import com.peculiaruc.alc_mmsystem_mentormanager.data.models.DocumentDto
 import com.peculiaruc.alc_mmsystem_mentormanager.databinding.FragmentProfileBinding
+import com.peculiaruc.alc_mmsystem_mentormanager.ui.adapters.DocumentsAdapter
+import com.peculiaruc.alc_mmsystem_mentormanager.ui.adapters.DocumentsProfileAdapter
 import com.peculiaruc.alc_mmsystem_mentormanager.ui.viewmodels.ProfileViewModel
 
 class ProfileFragment : Fragment() {
@@ -20,6 +27,33 @@ class ProfileFragment : Fragment() {
     ): View? {
         binding = FragmentProfileBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
+
+        Glide.with(this).load(R.mipmap.ic_launcher)
+            .circleCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(binding!!.imageViewProfileImg)
+
+        val fakeDocs = listOf(
+            DocumentDto(1, "Myresume.pdf"),
+            DocumentDto(2, "my certificate.doc"),
+            DocumentDto(3, "certificate2.doc"),
+        )
+
+        val documentsProfileAdapter = DocumentsProfileAdapter {
+
+        }
+
+        binding?.recyclerViewDocuments?.adapter = documentsProfileAdapter
+
+        documentsProfileAdapter.submitList(fakeDocs)
+
+        binding?.buttonEditProfile?.setOnClickListener {
+            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToEditProfileFragment())
+        }
+
+        binding?.topbar?.imageViewBack?.setOnClickListener {
+            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToRegisterFormFourFragment())
+        }
 
         return binding?.root
     }
