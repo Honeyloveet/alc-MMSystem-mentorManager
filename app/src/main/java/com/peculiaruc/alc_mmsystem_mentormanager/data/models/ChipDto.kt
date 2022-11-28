599ba48e23617060e47eacc804960798636f6885
Samuel Kebede <samuelkebede2000@gmail.com>
2022-11-29 01:38:39 +0300

    Update ChipDto.kt
    Added Documentation.

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/data/models/ChipDto.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/data/models/ChipDto.kt
@@ -1,3 +1,9 @@
 package com.peculiaruc.alc_mmsystem_mentormanager.data.models
 
+/**
+ * This data class holds the [id] and [name] for the chips
+ * of the previous programs held and technical proficiency
+ * which we get from the back end and dynamically add them
+ * to the view
+ */
 data class ChipDto(var id: Int , var name : String)
